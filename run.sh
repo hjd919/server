b3ab8610defa9957f45800d521155eaf152d5e3c
hjd919 <hjd919@126.com>
2019-07-18 14:47:50 +0800

    提交代码

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+type=$1
 
-go run cmd/main.go -conf configs
+case "$type" in
+"git")
+    git add -A
+    git commit -m '提交代码' -a
+    git pull
+    git push
+    ;;
+"run")
+    git pull
+
+    go build -o cmd/main cmd/main.go
+    chmod +x cmd/main
+    ./cmd/main -conf ../configs
+    ;;
+"deploy")
+    go run cmd/main.go -conf configs
+    ;;
+*)
+    echo "error"
+    ;;
+esac

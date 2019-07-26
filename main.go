6cefe9d7ac4de21549d9c4231bf2c74abf02583a
胡建德 <hujiande@hujiandedeMacBook-Pro.local>
2019-07-27 00:01:53 +0800

    ok

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/hjd919/server/api"
+	"github.com/hjd919/server/model"
+)
+
+func main() {
+	user := api.UserAddReq{}
+	println(user)
+	u := model.User{}
+	println(u)
+}

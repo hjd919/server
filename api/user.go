e91206cfbda670048e255fdcafb2fe88002682a4
胡建德 <hujiande@hujiandedeMacBook-Pro.local>
2019-07-27 00:26:42 +0800

    ok

--- api/user.go
+++ api/user.go
@@ -1,14 +1,8 @@
 package api
 
-import "fmt"
-
 type UserAddReq struct {
 	Username string                 `json:"username"`
 	Nickname string                 `json:"nickname"`
 	Avatar   string                 `json:"avatar"`
 	Profile  map[string]interface{} `json:"profile"`
-}
-
-func Test() {
-	fmt.Println("helloTest")
 }
